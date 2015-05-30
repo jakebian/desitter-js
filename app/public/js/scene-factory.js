eddc7b3e862cae715b130551970ba34eaa5fbac1
Jake <findjakebian@gmail.com>
2015-05-29 18:21:52 -0700

    refactor: make mesh construction code more functional)

--- app/public/js/scene-factory.js
+++ app/public/js/scene-factory.js
@@ -19,78 +19,71 @@
 
     function getSurfaceMesh(surface) {
 
+        /**
+         * Geometry initialization
+         */
+
         var geometry = new THREE.Geometry();
 
-        updateGeometry(geometry, surface);
+        addSurfaceToGeometry(geometry, surface);
+
         geometry.verticesNeedUpdate = true;
         geometry.elementsNeedUpdate = true;
         geometry.normalsNeedUpdate = true;
 
+        /**
+         * Mesh initialization
+         */
+
         var surfaceMesh = new THREE.Mesh(geometry, new THREE.MeshNormalMaterial({transparent: true, opacity: 0.8 }));
         surfaceMesh.doubleSided = true;
+
         return surfaceMesh;
     }
 
-    function updateGeometry(geometry, surface) {
-        /**
-         * Add vertices
-         */
+    function addSurfaceToGeometry(geometry, surface) {
 
-        for (var vertexIdx = 0; vertexIdx < surface.positions.length; vertexIdx++) {
+        surface.positions.forEach(addVertexToGeometry);
+        surface.cells.forEach(addFaceToGeometry);
+        geometry.faces.forEach(setFaceNormal);
 
-            var vertexPos = surface.positions[vertexIdx];
+
+        function addVertexToGeometry(vertexPos) {
             geometry.vertices.push(
                 new THREE.Vector3(vertexPos[0], vertexPos[1], vertexPos[2])
             );
-
         }
 
-        /**
-         * Add faces
-         */
+        function addFaceToGeometry(faceCell) {
+            geometry.faces.push(
+                new THREE.Face3(faceCell[0], faceCell[1], faceCell[2])
+            );
+        }
 
-        for(var faceIdx = 0; faceIdx < surface.cells.length; faceIdx++) {
-            var faceCell = surface.cells[faceIdx];
-            if(faceCell.length === 3) {
-                geometry.faces.push(new THREE.Face3(faceCell[0], faceCell[1], faceCell[2]));
-            } else if(faceCell.length === 4) {
-                geometry.faces.push(new THREE.Face4(faceCell[0], faceCell[1], faceCell[2], faceCell[3]));
+        function setFaceNormal(face) {
+
+            var vertices = {
+                A: geometry.vertices[face.a],
+                B: geometry.vertices[face.b],
+                C: geometry.vertices[face.c]
             }
-        }
 
-        /**
-         * Normals
-         */
-        var cb = new THREE.Vector3(), ab = new THREE.Vector3();
-        for (var i=0; i < geometry.faces.length; ++i) {
-          var f = geometry.faces[i];
-          var vA = geometry.vertices[f.a];
-          var vB = geometry.vertices[f.b];
-          var vC = geometry.vertices[f.c];
-          cb.subVectors(vC, vB);
-          ab.subVectors(vA, vB);
-          cb.cross(ab);
-          cb.normalize();
-          if (surface.cells[i].length == 3) {
-            f.normal.copy(cb)
-            continue;
-          }
+            var edgeCB = new THREE.Vector3();
+            edgeCB.subVectors(vertices.C, vertices.B);
 
-          // quad
-          if (cb.isZero()) {
-            // broken normal in the first triangle, let's use the second triangle
-            var vA = geometry.vertices[f.a];
-            var vB = geometry.vertices[f.c];
-            var vC = geometry.vertices[f.d];
-            cb.subVectors(vC, vB);
-            ab.subVectors(vA, vB);
-            cb.cross(ab);
-            cb.normalize();
-          }
-          f.normal.copy(cb);
+            var edgeAB = new THREE.Vector3();
+            edgeAB.subVectors(vertices.A, vertices.B);
+
+            var normal = new THREE.Vector3();
+            normal.crossVectors(edgeCB, edgeAB);
+            normal.normalize();
+
+            face.normal.copy(normal)
         }
 
     }
+
+
 
     function getCircleMesh() {
 
